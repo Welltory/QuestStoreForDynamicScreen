cfd2d9b8fe1e2106af7b30ec74bd0f846fb0ea75
Andrew Lazarev <andrej.lazarev@gmail.com>
2017-09-14 11:56:57 +0500

    Update quest_preview_available.js
    Fix resource path

--- quest_preview_available.js
+++ quest_preview_available.js
@@ -1,5 +1,5 @@
 module.exports = {
     "title": "Available quest",
     "background": require("./image_preview_background.js"),
-    "action": require("/Users/andrejlazarev/Desktop/Dynamic layout/action_page_activate.js")
+    "action": require("./action_page_activate.js")
 }
