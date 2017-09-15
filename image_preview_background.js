547313f6abdee5fa475aaf5ad009a7ef525804b1
Андрей Лазарев <andrej.lazarev@gmail.com>
2017-09-15 17:34:46 +0500

    Update preview images

--- image_preview_background.js
+++ image_preview_background.js
@@ -1,6 +1,6 @@
 module.exports = {
     "height": 104,
-    "url": "https://ucarecdn.com/39155edb-d210-4522-914c-a440d84c5135/",
+    "url": "https://ucarecdn.com/e7832fdb-8b4e-4178-9a24-c35bb4856a5e/",
     "width": 104,
-    "uploadcare_id": "39155edb-d210-4522-914c-a440d84c5135"
+    "uploadcare_id": "e7832fdb-8b4e-4178-9a24-c35bb4856a5e"
 }
