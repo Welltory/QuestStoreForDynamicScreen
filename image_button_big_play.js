a79cd84d89356c58ea41c10e00060dcf01655493
Андрей Лазарев <andrej.lazarev@gmail.com>
2017-09-15 15:25:11 +0500

    Fix sizes

--- image_button_big_play.js
+++ image_button_big_play.js
@@ -1,6 +1,6 @@
 module.exports = {
-    "height": 29,
+    "height": 32,
     "url": "https://ucarecdn.com/b6508341-35f6-4671-81ba-93bfe7010d19/",
-    "width": 23,
+    "width": 32,
     "uploadcare_id": "b6508341-35f6-4671-81ba-93bfe7010d19"
 }
