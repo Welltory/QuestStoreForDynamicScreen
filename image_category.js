fe135e8409a9d8790a85fdb07d3ac3cddf42f0ca
Андрей Лазарев <andrej.lazarev@gmail.com>
2017-09-15 18:12:16 +0500

    Fix category image

--- image_category.js
+++ image_category.js
@@ -1,6 +1,6 @@
 module.exports = {
-        "url": "asdfasfdsfasdf",
-        "uploadcare_id" : "",
+        "url": "https://ucarecdn.com/e7832fdb-8b4e-4178-9a24-c35bb4856a5e/",
+        "uploadcare_id" : "e7832fdb-8b4e-4178-9a24-c35bb4856a5e",
         "width": 25,
         "height": 25
 }
